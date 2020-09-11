1bb6e52e52f8f79c14206b4dc5a461320d3caa00
Chrish0r <Christof.Gadek10@googlemail.com>
2020-09-11 11:59:32 +0200

    updated CONNECTION_STRING and inserted contacts into table

--- DatabaseTest/src/test/Main.java
+++ DatabaseTest/src/test/Main.java
@@ -6,10 +6,11 @@ public class Main {
 	
 	public static final String DB_NAME = "kontakte.de";
 	// Falls DB noch nicht exisitiert, wird sie dort erstellt
-	public static final String CONNECTION_STRING = "jdbc:sqlite:C:\\Users\\User\\Desktop\\Studium-OTH\\Lernplan\\Semester-4\\Java_DB_Kurs\\DatabaseCourse\\DatabaseTest\\src\\test\\" + DB_NAME;
+	public static final String CONNECTION_STRING = "jdbc:sqlite:C:\\Repositories\\DatabaseCourse\\DatabaseTest\\src\\test\\" + DB_NAME;
 	
 	// Name der Tabellen in unserer Datenbank
 	public static final String TABLE_KONTAKT = "kontakte";
+
 	public static final String COLUMN_NAME = "name";
 	public static final String COLUMN_PHONE = "phone";
 	public static final String COLUMN_EMAIL = "email";
@@ -21,6 +22,13 @@ public class Main {
 			Connection connection = DriverManager.getConnection(url);
 			
 			Statement statement = connection.createStatement();
+
+			statement.execute("CREATE TABLE IF NOT EXISTS " + TABLE_KONTAKT + "( " + COLUMN_NAME + " TEXT, "
+			                    + COLUMN_PHONE + " INTEGER, " + COLUMN_EMAIL + " TEXT)"); // In Konsole noch ein Semicolon nach schließender SQL-Klamme
+
+			// create contact
+			createContact(statement, "Christian", 123456, "Christian@gmail.com");
+			createContact(statement, "Chris", 152123456, "Chris@web.de");
 			
 			statement.close();
 			connection.close();
@@ -33,10 +41,20 @@ public class Main {
     /**
      * CRUD Methoden
      */
-    
-    private static void createContact() {
-    	
+/*
+	private static void createContact(Statement statement, String name, int phone, String email) throws SQLException  {
+		statement.execute("INSERT INTO " + TABLE_KONTAKT + " VALUES " + "('" + name + "'," + phone + ",'" + email + "')");
+	}
+
+ */
+
+// VORSICHT: single quotes needed.
+    private static void createContact(Statement statement, String name, int phone, String email) throws SQLException {
+		statement.execute("INSERT INTO " + TABLE_KONTAKT + " VALUES " + "('"
+				+ name + "', " + phone + ",' " + email + "')");
     }
+
+
     
  private static void readContact() {
     	
